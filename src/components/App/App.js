8e3bdc704f672156593145af851ae0d87e87a050
neretin-trike <hawktrike@gmail.com>
2019-03-04 23:19:08 +0700

    update(js,css): добавление вывода списка посетителей

---
+++ src/components/App/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AdressList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:22080/api/address/addresses", {method:"GET"})
+      .then( res => res.json() )
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Имя</th>
+              <th>Фамилия</th>
+              <th>Отчество</th>
+              <th>Адрес</th>
+              <th>Номер</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr key={item.index}>
+                  <td>{item.name}</td>
+                  <td>{item.surname}</td>
+                  <td>{item.middlename}</td>
+                  <td>{item.adress}</td>
+                  <td>{item.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Адресная книга посетителей</h2>
+        </header>
+        <main className="App-main">
+          <AdressList />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
