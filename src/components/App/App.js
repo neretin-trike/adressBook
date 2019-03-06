7175f7943f5031ee407571ab313aa8d99bab48da
Artem Neretin <hawktrike@gmail.com>
2019-03-06 15:00:36 +0700

    update(js): добавление возможности создания новых записей

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,7 +33,6 @@ class AddressList extends Component {
       .then( res => res.json() )
       .then(
         (result) => {
-          console.dir(result[0]);
           this.setState({
             isLoaded: true,
             items: result
@@ -47,18 +46,36 @@ class AddressList extends Component {
         }
       )
   }
-  addRow() {
+  addRow(object) {
     let items = this.state.items;
-    items.push();
+    items.push(object);
     this.setState({items: items});
+
+    this.props.onAddItem(null);
+
+    let formData = new FormData;
+    formData.append("item", object);
+    fetch("http://localhost:22080/api/address/add", {method:"POST", body: formData})
+      .then( res => res.json() )
+      .then(
+        (result) => {
+          alert(result);
+        },
+        (error) => {
+          alert(error);
+        }
+      )
   }
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, items, newItem } = this.state;
     if (error) {
       return <div>Ошибка: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Загрузка...</div>;
     } else {
+      if (this.props.newItem !== null) {
+        this.addRow(this.props.newItem);
+      }
       return (
         <table className="Address-Table" >
           <thead>
@@ -86,7 +103,6 @@ class ModalDialog extends Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     const {children} = this.props;
     return (
@@ -101,14 +117,11 @@ class ModalDialog extends Component {
 class AddItemButton extends Component {
   constructor(props) {
     super(props);
-
     this.onClickHandle = this.onClickHandle.bind(this);
   }
-
   onClickHandle(e) {
     this.props.onModalShow();
   }
-
   render() {
     return (
       <button onClick={this.onClickHandle} className="Add-Item Button" >{this.props.name}</button>
@@ -121,11 +134,13 @@ class ModalWindow extends Component {
     super(props);
     this.onClickHandle = this.onClickHandle.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.onAddItem = this.onAddItem.bind(this);
 
     this.state = {
-      firstName: "1",
-      lastName: "2",
-      patronymicName: "3",
+      index: Math.round(Math.random()*(1000-1)+1),
+      name: "1",
+      surname: "2",
+      middlename: "3",
       address: "4",
       phone: "5"
     }
@@ -138,19 +153,24 @@ class ModalWindow extends Component {
   onClickHandle(e) {
     this.props.onModalClose();
   }
+  onAddItem(e) {
+    this.props.onAddItem(this.state);
+    this.props.onModalClose();
+    e.preventDefault();
+  }
   render() {
     return (
       <section className="Modal-Window">
         <h3>Добавить новую запись</h3>
         <form>
-          <label>Имя<input name="firstName" value={this.state.firstName} onChange={this.onInputChange}/></label>
-          <label>Фамилия<input name="lastName" value={this.state.lastName} onChange={this.onInputChange}/></label>
-          <label>Отчество<input name="patronymicName" value={this.state.patronymicName} onChange={this.onInputChange}/></label>
+          <label>Имя<input name="name" value={this.state.name} onChange={this.onInputChange}/></label>
+          <label>Фамилия<input name="surname" value={this.state.surname} onChange={this.onInputChange}/></label>
+          <label>Отчество<input name="middlename" value={this.state.middlename} onChange={this.onInputChange}/></label>
           <label>Адрес<input name="address" value={this.state.address} onChange={this.onInputChange}/></label>
           <label>Номер<input name="phone" value={this.state.phone} onChange={this.onInputChange}/></label>
           <nav className="ModalBtn-nav">
             <input className="Button" type="button" onClick={this.onClickHandle} value="Закрыть"/>
-            <input className="Button" type="submit" value="Добавить"/>
+            <input className="Button" type="submit" onClick={this.onAddItem} value="Добавить"/>
           </nav>
         </form>
       </section>
@@ -164,10 +184,12 @@ class App extends Component {
 
     this.state = {
       showModal: true,
+      item: null,
     };
 
     this.onModalShow = this.onModalShow.bind(this);
     this.onModalClose = this.onModalClose.bind(this);
+    this.onAddItem = this.onAddItem.bind(this);
   }
 
   onModalClose(){
@@ -176,10 +198,12 @@ class App extends Component {
   onModalShow(){
     this.setState({showModal: true});
   }
-  
-  render() {
-    const { showModal } = this.state;
+  onAddItem(object){
+    this.setState({item: object});
+  }
 
+  render() {
+    const { showModal, item } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -188,7 +212,7 @@ class App extends Component {
         <main className="App-main">
           <article className="Container">
               <section className="Address-List" >
-                <AddressList />
+                <AddressList onAddItem={this.onAddItem} newItem={item}/>
               </section>
               <nav className="Item-nav">
                 <AddItemButton onModalShow={this.onModalShow} showModal={showModal} name="Добавить запись"/>
@@ -197,7 +221,7 @@ class App extends Component {
         </main>
         {showModal &&
           <ModalDialog>
-            <ModalWindow onModalClose={this.onModalClose} />
+            <ModalWindow onAddItem={this.onAddItem} onModalClose={this.onModalClose} />
           </ModalDialog>}
       </div>
     );
